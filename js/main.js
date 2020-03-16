6f01a69ca734b8a79d107bd001b9dab31dc45467
Davide <davide.roberti89@gmail.com>
2020-03-16 14:27:47 +0100

    cambio colori al click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 
+$('.square').click(function(){
+    var cliccato = $(this);
+    var numeroUnivoco = $(this).children('span').text();
+    // console.log(numeroUnivoco);
+    $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/random/int',
+        method: 'GET',
+        success: function (data) {
+            var numeroRandom = data.response;
+            decisioneColore(numeroRandom);
+        },
+        error: function () {
+            alert('ERROR!!!!!!')
+        }
+    });
+    function decisioneColore (numRandom) {
+        if (numRandom <= 5) {
+            cliccato.addClass('giallo');
+        } else {
+            cliccato.addClass('verde');
+        }
+    };
+});
